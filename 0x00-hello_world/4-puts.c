a0ea70f5debf41d956212c06c49cc95c4dd0e0ac
dickmwambula <dickmwambula@gmail.com>
2023-03-17 12:37:58 -0700

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main fuunction
- * Return: always 0
+ * main - this is the main function
+ *
+ * Return:always 0
  */	
-
-int main(void)
+int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle);
-	return(0);    
+	return(0);
 }
