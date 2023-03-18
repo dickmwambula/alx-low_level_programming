51f93168acc77933eb974f9728a678d7b935d369
dickmwambula <dickmwambula@gmail.com>
2023-03-18 06:29:12 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fputs("and thet piece of art is usefiul\"-Dora Korpar,2015-10-19\n", stdout);
+	fputs("and thet piece of art is useful\"-Dora Korpar,2015-10-19\n", stdout);
 	return (1);
 }
