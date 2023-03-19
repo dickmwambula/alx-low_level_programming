058331a598448333a53ecc22b3d977c0ed6ccedf
dickmwambula <dickmwambula@gmail.com>
2023-03-19 01:04:37 -0700

    Task 2 Printing Alphabet in Lower Case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
