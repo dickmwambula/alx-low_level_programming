330543f7becc309f490f29e9b245c68ab08ff1ca
dickmwambula <dickmwambula@gmail.com>
2023-03-19 02:51:50 -0700

    Task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that will print a single number to base ten
+ *
+ * Return:0 (Successful)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
