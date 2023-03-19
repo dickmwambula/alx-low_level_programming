6ac49727188d9070e26579630652d9533eaa933f
dickmwambula <dickmwambula@gmail.com>
2023-03-19 10:37:40 -0700

    Combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,7 +23,7 @@ int main(void)
 					putchar(a + '0');
 					putchar(b + '0');
 					putchar(c + '0');
-					if (a + c + c!= 24)
+					if (a + b + c != 24)
 					{
 						putchar(',');
 						putchar(' ');
