9a1839f2aca428ecbae833ecf1061c5d573c53d5
dickmwambula <dickmwambula@gmail.com>
2023-03-20 08:48:01 -0700

    For reals software enginnering is eat me

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main  - Main function
+ *
+ *Return:0 (successful)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; a < 98 ; a++)
+	{
+	for (b = 1 ; b < 99 ; b++)
+	{
+	putchar((a / 10) + '0');
+	putchar((b % 10) + '0');
+	putchar(' ');
+	putchar((a / 10) + '0');
+	putchar((b % 10) + '0');
+	}
+	if (a == 98 && b == 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
