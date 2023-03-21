e34053cf5a36fbbe8b1913c3160b83a199620b53
dickmwambula <dickmwambula@gmail.com>
2023-03-21 12:08:34 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * Main  - A program that print putchar
- * is written by Dickson
+ * main   - this the main  function
+ * Desciption - A program is written by Dickson
  * Return: 0 (success)
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
